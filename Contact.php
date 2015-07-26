720670456b2dc596b396dd8315649d41a0323cde
MaxMackinw1 <macmackinw1@gmail.com>
2015-07-26 10:29:33 -0400

    test php scirpt working

--- Contact.php
+++ Contact.php
@@ -1,3 +1,45 @@
+<?php
+    if (isset($_POST["submit"])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = 'Contact from your Webpage'; 
+        $to = 'macmackinw1@gmail.com'; 
+        $subject = 'Message from your WebSite ';
+        
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+        // Check if name has been entered
+        if (!$_POST['name']) {
+            $errName = 'Please enter your name';
+        }
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human !== 4) {
+            $errHuman = 'Your anti-spam is incorrect';
+        }
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+    if ($mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+    }
+}
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,7 +61,7 @@
     <link href="css/style.css" rel="stylesheet">
 	<link href="css/Contact.css" rel="stylesheet">
 	<link href="css/bootstrap-social.css" rel="stylesheet">
-	<?php include "php/custommail.php";?>
+	<?php include 'php/custommail.php';?>
 	
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
